3ae82318a2fe0eaafcb12d304979a927e0d1bce4
kanzariamine <kanzari.amin@gmail.com>
2018-11-17 23:16:58 +0100

    Test the API work in console log

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ let factText = document.querySelector('#factText');
 let numberInput = document.querySelector('#numberInput');
 numberInput.addEventListener('input', getFact);
 
+async function getFact() {
+  let number = numberInput.value;
+
+  let res =  await fetch(`http://numbersapi.com/${number}`);
+  let data = await res.text();
+  console.log(data)
+}
