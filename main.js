92d7d402b1083ce057f6512878d89c3a6a1b639f
kanzariamine <kanzari.amin@gmail.com>
2018-11-17 23:15:14 +0100

    Select element from the UI

---
+++ main.js
@@ -0,0 +1,6 @@
+let fact = document.querySelector('#fact');
+let factText = document.querySelector('#factText');
+
+let numberInput = document.querySelector('#numberInput');
+numberInput.addEventListener('input', getFact);
+
